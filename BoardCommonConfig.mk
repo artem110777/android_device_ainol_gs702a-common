75d390569a345327edd54a5c5f39dbdc827c4d97
iBullRay <bullray097@yandex.ru>
2015-02-08 15:18:52 +0200

    Initial commit for CM-11.0

---
+++ BoardCommonConfig.mk
@@ -0,0 +1,100 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/ainol/gs702a-common
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a5
+TARGET_CPU_VARIANT := cortex-a5
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+BOARD_VENDOR := ainol
+TARGET_BOARD_PLATFORM := ATM702X
+TARGET_BOARD_PLATFORM_GPU := VIVANTE
+TARGET_SOC := gs702a
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB=false
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := ATM702X
+TARGET_NO_BOOTLOADER := true
+
+# Camera
+USE_CAMERA_STUB := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# Filesystem
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 536870912
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Graphics
+BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+BOARD_USES_HGL := true
+BOARD_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+BOARD_USE_SKIA_LCDTEXT := true
+TARGET_BOOTANIMATION_PRELOAD := true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel/kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyS5,115200 earlyprintk androidboot.selinux=0
+BOARD_PAGE_SIZE := 2048
+
+# Radio
+TARGET_NO_RADIOIMAGE := true
+
+# Recovery
+# CWM:
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
+RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_565
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+BOARD_HAS_NO_SELECT_BUTTON := true
+# TWRP:
+RECOVERY_SDCARD_ON_DATA := false
+BOARD_HAS_NO_REAL_SDCARD := false
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+HAVE_SELINUX := false
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INTERNAL_STORAGE_PATH := "/dev/block/actj"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "/sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/dev/block/mmcblk0"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "/sd-ext"
+TW_DEFAULT_EXTERNAL_STORAGE := false
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/act_pwm_backlight/brightness"
+
+# Releasetool
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := build/tools/releasetools/ota_from_target_files
---
+++ common.mk
@@ -0,0 +1,129 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/ainol/gs702a-common
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Audio configuration file
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy.conf:/system/etc/audio_policy.conf
+
+# Feature live wallpaper
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml	
+	
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    static_busybox \
+    make_ext4fs \
+    e2fsck \
+    mke2fs \
+    ping \
+    strace \
+    com.android.future.usb.accessory \
+    charger \
+    charger_res_images \
+    usbmond
+
+# GSensor
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
+	
+# Init files
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/rootdir/lib/modules/atv5202_mmc.ko:root/lib/modules/atv5202_mmc.ko \
+    $(LOCAL_PATH)/rootdir/lib/modules/blk1drv.ko:root/lib/modules/blk1drv.ko \
+    $(LOCAL_PATH)/rootdir/lib/modules/blk2drv.ko:root/lib/modules/blk2drv.ko \
+    $(LOCAL_PATH)/rootdir/lib/modules/mmc_core.ko:root/lib/modules/mmc_core.ko \
+    $(LOCAL_PATH)/rootdir/lib/modules/tsd_block.ko:root/lib/modules/tsd_block.ko \
+    $(LOCAL_PATH)/rootdir/fstab.gs702a:root/fstab.gs702a \
+    $(LOCAL_PATH)/rootdir/fstab.sdboot.gs702a:root/fstab.sdboot.gs702a \
+    $(LOCAL_PATH)/rootdir/init.gs702a.rc:root/init.gs702a.rc \
+    $(LOCAL_PATH)/rootdir/init.gs702a.sdboot.rc:root/init.gs702a.sdboot.rc \
+    $(LOCAL_PATH)/rootdir/init.gs702a.usb.rc:root/init.gs702a.usb.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.gs702a.rc:root/ueventd.gs702a.rc \
+    $(LOCAL_PATH)/rootdir/usbmond.sh:root/usbmond.sh
+	
+# Keylayout and Idc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/atc260x-adckeypad.kl:system/usr/keylayout/atc260x-adckeypad.kl \
+    $(LOCAL_PATH)/idc/ft5x0x_ts.idc:system/usr/idc/ft5x0x_ts.idc \
+    $(LOCAL_PATH)/idc/GT813.idc:system/usr/idc/GT813.idc \
+    $(LOCAL_PATH)/idc/mt395.idc:system/usr/idc/mt395.idc
+	
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    librs_jni	
+	
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+# OMX
+PRODUCT_PACKAGES += \
+    libstagefrighthw
+
+# OMX configuration file
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/omx_codec.xml:system/etc/omx_codec.xml
+	
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mass_storage
+	
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+	
+# Vold and Storage
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/configs/vold.sdboot.fstab:system/etc/vold.sdboot.fstab	
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.opengles.version=131072 \
+    hwui.render_dirty_regions=false
+
+PRODUCT_TAGS += dalvik.gc.type-precise	
+
+PRODUCT_CHARACTERISTICS := tablet
